e15a830c28f8d394504bce5042323c9defb70f7e
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-03-28 06:18:14 +0100

    updated react version

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-// take the react component and show it on the screen
-ReactDOM.render(
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
