8d234cb827c17f19a9cc9038b0536af949894b55
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-04-08 22:15:13 +0100

    added title to header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,8 +10,7 @@ import {
 } from "@material-ui/core";
 import { ShoppingCart } from "@material-ui/icons";
 import useStyles from "./styles";
-import logo from '../../asset/logo.png'
-
+import logo from "../../asset/logo.png";
 
 const Navbar = () => {
   const classes = useStyles();
@@ -21,8 +20,13 @@ const Navbar = () => {
       <AppBar position="fixed" className={classes.appBar} color="inherit">
         <Toolbar>
           <Typography variant="h6" color="inherit" className={classes.title}>
-            <img src={logo} alt="Tech-Shop" height="25px" className={classes.image} />
-            E-Store
+            <img
+              src={logo}
+              alt="Tech-Shop"
+              height="25px"
+              className={classes.image}
+            />
+            E-STORE
           </Typography>
           <div className={classes.grow} />
           <div className={classes.button}>
