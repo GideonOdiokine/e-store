f099ee8c340c7c17276cfc92b4190a32a61d692f
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2022-07-20 01:13:19 +0100

    added fecthShippingCountries and  fetchSubdivisions

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
 	InputLabel,
 	Select,
@@ -11,7 +11,7 @@ import { useForm, FormProvider } from "react-hook-form";
 import { commerce } from "../../lib/commerce";
 import FormInput from "./FormInput";
 
-const AddressForm = () => {
+const AddressForm = ({ checkoutToken }) => {
 	const [shippingCountries, setShippingCountries] = useState([]);
 	const [shippingCountry, setShippingCountry] = useState("");
 	const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
@@ -19,13 +19,41 @@ const AddressForm = () => {
 	const [shippingOptions, setShippingOptions] = useState([]);
 	const [shippingOption, setShippingOption] = useState("");
 	const methods = useForm();
+	const countries = Object.entries(shippingCountries).map(([code, name]) => ({
+		id: code,
+		label: name,
+	}));
+	const subdivisions = Object.entries(shippingSubdivisions).map(
+		([code, name]) => ({
+			id: code,
+			label: name,
+		})
+	);
 
 	const fetchShippingCountries = async (checkoutTokenId) => {
 		const { countries } = await commerce.services.localeListShippingCountries(
 			checkoutTokenId
 		);
+
 		setShippingCountries(countries);
+		setShippingCountry(Object.keys(countries)[0]);
 	};
+
+	const fetchSubdivisions = async (countryCode) => {
+		const { subdivisions } = await commerce.services.localeListSubdivisions(
+			countryCode
+		);
+		setShippingSubdivisions(subdivisions);
+		setShippingSubdivision(Object.keys(subdivisions)[0]);
+	};
+
+	useEffect(() => {
+		fetchShippingCountries(checkoutToken?.id);
+	}, []);
+
+	useEffect(() => {
+		if (shippingCountry) fetchSubdivisions(shippingCountry);
+	}, [shippingCountry]);
 
 	return (
 		<>
@@ -43,28 +71,42 @@ const AddressForm = () => {
 						<FormInput required name='zip' label='Zip / Postal Code' />
 						<Grid item xs={12} sm={6}>
 							<InputLabel>Shipping Country</InputLabel>
-							<Select value='' fullWidth onChange>
-								<MenuItem key='{}' value='{}'>
-									Select Me
-								</MenuItem>
+							<Select
+								value={shippingCountry}
+								fullWidth
+								onChange={(e) => setShippingCountry(e.target.value)}>
+								{countries.map((country) => (
+									<MenuItem
+										key={country.id}
+										value={country.id ? country.id : ""}>
+										{country.label}
+									</MenuItem>
+								))}
 							</Select>
 						</Grid>
 						<Grid item xs={12} sm={6}>
 							<InputLabel>Shipping Subdivision</InputLabel>
-							<Select value='' fullWidth onChange>
-								<MenuItem key='{}' value='{}'>
-									Select Me
-								</MenuItem>
+							<Select
+								value={shippingSubdivision}
+								fullWidth
+								onChange={(e) => setShippingSubdivision(e.target.value)}>
+								{subdivisions.map((subdivision) => (
+									<MenuItem
+										key={subdivision.id}
+										value={subdivision.id ? subdivision.id : ""}>
+										{subdivision.label}
+									</MenuItem>
+								))}
 							</Select>
 						</Grid>
-						<Grid item xs={12} sm={6}>
+						{/* <Grid item xs={12} sm={6}>
 							<InputLabel>Shipping Options</InputLabel>
 							<Select value='' fullWidth onChange>
 								<MenuItem key='{}' value='{}'>
 									Select Me
 								</MenuItem>
 							</Select>
-						</Grid>
+						</Grid>  */}
 					</Grid>
 				</form>
 			</FormProvider>
